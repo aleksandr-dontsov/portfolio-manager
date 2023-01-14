044580e2d666a9bda45650ee52b9d94bd6fdabd7
Aleksandr Dontsov <alexdontsov2008@ya.ru>
2023-01-14 16:53:58 +0100

    Setup PostgreSQL database

--- services/web/app.py
+++ services/web/app.py
@@ -1,59 +1,60 @@
-from crypt import methods
-from flask import Flask, jsonify, request
-
-import datetime
+from flask import (
+    Flask,
+    jsonify,
+    # The glogal request object to access submitted data
+    request
+)
+from db import (
+    init_db,
+    get_db_connection
+)
 
 app = Flask(__name__)
 
-portfolios_data = [
-    {
-        "potfolio_id": 0,
-        "portfolio_name": "Tech conpanies",
-        "user_id": 0,
-        "currency_id": 0,
-        "created_date": datetime.datetime.now().date()
-    },
-    {
-        "potfolio_id": 1,
-        "portfolio_name": "ETFs",
-        "user_id": 0,
-        "currency_id": 0,
-        "created_date": datetime.datetime.now().date()
-    },
-]
-
-# Portfolio
-# ################
-# portfolio_id
-# portfolio_name
-# user_id
-# currency_id
-# created_date
-
-# Currency table
-# ################
-# currency_code
-# currency_name
-
 @app.route('/portfolios', methods=['GET'])
 def portfolios():
-    return jsonify(portfolios_data)
+    with get_db_connection() as connection:
+        with connection.cursor() as cursor:
+            cursor.execute('SELECT * FROM portfolio;')
+            portfolios = cursor.fetchall()
+    return jsonify(portfolios), 200
+
 
 @app.route('/portfolios', methods=['POST'])
 def add_portfolio():
     portfolio = request.get_json()
-    portfolios_data.append(portfolio)
-    return {'id': len(portfolios_data)}, 200
+    with get_db_connection() as connection:
+        with connection.cursor() as cursor:
+            # Insert a portfolio
+            cursor.execute('INSERT INTO portfolio (portfolio_name, user_id, currency_id)'
+                                'VALUES(%s, %s, %s) RETURNING portfolio_id',
+                           (portfolio['portfolio_name'],
+                            portfolio['user_id'],
+                            portfolio['currency_id']))
+            connection.commit()
+            id = cursor.fetchone()[0]
 
-@app.route('/portfolios/<int:index>', methods=['PUT'])
-def update_portfolio(index: int):
-    portfolio = request.get_json()
-    portfolios_data[index] = portfolio
-    return jsonify(portfolios_data[index]), 200
+    return {'id': id}, 200
 
-@app.route('/portfolios/<int:index>', methods=['DELETE'])
-def delete_portfolio(index: int):
-    portfolios_data.pop(index)
-    return 'None', 200
+# @app.route('/portfolios/<int:index>', methods=['PUT'])
+# def update_portfolio(index):
+#     portfolio = request.get_json()
+#     portfolios_data[index] = portfolio
+#     return jsonify(portfolios_data[index]), 200
 
-app.run()
+# @app.route('/portfolios/<int:index>', methods=['DELETE'])
+# def delete_portfolio(index):
+#     portfolios_data.pop(index)
+#     return 'None', 200
+
+
+if  __name__ == '__main__':
+    # Init the database
+    init_db()
+
+    # 127.0.0.1 is a default value.
+    # Here we need to bind our service to the 0.0.0.0 
+    # to make it accessible to machines other than docker.
+    # Otherwise the service will be only accessible
+    # from the docker localhost
+    app.run(host="0.0.0.0")
